3c69fe1d337899c97f0176a43bc1fbfaa5395ecf
gorolykmaxim <streetbattle52@gmail.com>
2020-03-09 12:43:36 +0200

    Use re-usable BottomNavigationActivity from android-commons

--- app/src/main/java/com/example/timetrakr/MainActivity.java
+++ app/src/main/java/com/example/timetrakr/MainActivity.java
@@ -1,90 +1,27 @@
 package com.example.timetrakr;
 
 import android.os.Bundle;
-import androidx.annotation.NonNull;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.appcompat.app.AppCompatActivity;
-import android.util.SparseArray;
-import android.view.MenuItem;
 
 import com.example.timetrakr.view.activities.durations.ActivitiesDurationsFragment;
 import com.example.timetrakr.view.activities.started.ActivitiesStartedFragment;
+import com.gorolykmaxim.android.commons.bottomnavigation.BottomNavigationActivity;
 
 /**
  * Main activity of the application, that contains bottom navigation and a fragment container.
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BottomNavigationActivity {
 
-    private Integer selectedItemId;
+    public MainActivity() {
+        super(R.menu.navigation, R.id.navigation_activity_start_events);
+    }
 
     /**
      * {@inheritDoc}
      */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        addFragment(R.id.navigation_activity_start_events, new ActivitiesStartedFragment());
+        addFragment(R.id.navigation_activity_durations, new ActivitiesDurationsFragment());
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        BottomNavigationView navigation = findViewById(R.id.navigation);
-        BottomNavigationListener listener = new BottomNavigationListener(getSupportFragmentManager(), R.id.main_fragment_container);
-        listener.addFragment(R.id.navigation_activity_start_events, new ActivitiesStartedFragment());
-        listener.addFragment(R.id.navigation_activity_durations, new ActivitiesDurationsFragment());
-        navigation.setOnNavigationItemSelectedListener(listener);
-        if (selectedItemId == null) {
-            selectedItemId = R.id.navigation_activity_start_events;
-        }
-        navigation.setSelectedItemId(selectedItemId);
     }
-
-    /**
-     * Bottom navigation item selection listener.
-     */
-    private class BottomNavigationListener implements BottomNavigationView.OnNavigationItemSelectedListener {
-
-        private FragmentManager fragmentManager;
-        private int fragmentContainerId;
-        private SparseArray<Fragment> navigationItemIdToFragment;
-
-        /**
-         * Construct the listener.
-         *
-         * @param fragmentManager fragment manager used to display fragments
-         * @param fragmentContainerId id of the fragment container to display fragments in
-         */
-        public BottomNavigationListener(FragmentManager fragmentManager, int fragmentContainerId) {
-            navigationItemIdToFragment = new SparseArray<>();
-            this.fragmentManager = fragmentManager;
-            this.fragmentContainerId = fragmentContainerId;
-        }
-
-        /**
-         * Show specified fragment in the fragment container when user selects menu item with
-         * the specified ID in the bottom navigation.
-         *
-         * @param menuItemId ID of the menu item
-         * @param fragment fragment to display
-         */
-        public void addFragment(int menuItemId, Fragment fragment) {
-            navigationItemIdToFragment.put(menuItemId, fragment);
-        }
-
-        /**
-         * {@inheritDoc}
-         */
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-            Fragment fragmentToDisplay = navigationItemIdToFragment.get(menuItem.getItemId());
-            if (fragmentToDisplay != null) {
-                fragmentManager
-                        .beginTransaction()
-                        .replace(fragmentContainerId, fragmentToDisplay)
-                        .commit();
-                return true;
-            } else {
-                return false;
-            }
-        }
-    }
-
 }
